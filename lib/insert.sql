ed7fc459c734f42cd578ee24d340652189e5aa59
colinhalexander <chalexander@icloud.com>
2019-09-04 14:41:44 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,70 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+    'Mr. Chocolate',
+    2,
+    'Male',
+    'Brown',
+    'Chipper',
+    'TRUE'
+);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+    'Rowdy',
+    2,
+    'Male',
+    'Black',
+    'Hangry',
+    'TRUE'
+);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+    'Tabitha',
+    3,
+    'Female',
+    'Brown',
+    'Laidback',
+    'TRUE'
+);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+    'Sergeant Brown',
+    19,
+    'Female',
+    'Blue',
+    'Authoritarian',
+    'TRUE'
+);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+    'Melissa',
+    4,
+    'Female',
+    'Yellow',
+    'Quiet',
+    'False'
+);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+    'Grinch',
+    10,
+    'Male',
+    'Green',
+    'A Mean One',
+    'TRUE'
+);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+    'Wendy',
+    5,
+    'Female',
+    'Red',
+    'Vengeful',
+    'TRUE'
+);
+
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (
+    6,
+    'Male',
+    'Purple',
+    'Murderous',
+    'TRUE'
+);
